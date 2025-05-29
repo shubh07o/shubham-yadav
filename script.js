fb374220615d5db49804c1322e6a5cd2ac602b38
shubh07o <rowdyji59@gmail.com>
2025-05-29 10:13:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ function showMessage() {
   const message = document.getElementById('message');
   message.textContent = "You're doing amazing, Shubham! Keep coding. 🔥💻";
 }
+const totalImages = 21;
+let currentImage = 1;
+const img = document.getElementById("slideshow");
+
+setInterval(() => {
+  currentImage = (currentImage % totalImages) + 1;
+  img.src = `assets/images/${currentImage}.jpg.jpg`;
+}, 3000); // changes every 3 seconds
